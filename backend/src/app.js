8004d430ce9ff180cbfa222ef698567fd49fb76c
anurag0577 <anurag0577@gmail.com>
2025-10-13 22:43:21 +0530

    setup a nodeJS project

---
+++ backend/src/app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import {connectDB} from './Database/index.js'
+
+// creating an instance of express
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()) // this line is important bcoz it helps to parse the incoming request body as json objects
+app.use(express.urlencoded({ extended: true })) // this line is important bcoz it helps to parse the incoming request body as urlencoded objects
+
+// enabling cors for cross origin resource sharing
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: [
+        'Content-Type', 
+        'Authorization',
+        'Cookie'
+    ],
+    optionsSuccessStatus: 200
+}))
+
+
+// demo api later we will remove it
+app.get('/', (req, res) => {
+    res.json('This is the homepage.')
+})
+
+// once the db is connected then only the server start running
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server start listning on ${PORT} `)
+})
+})
