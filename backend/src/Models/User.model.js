d01b6f892304e2a2c1e63a068a17168534e17b13
anurag0577 <anurag0577@gmail.com>
2025-10-14 00:06:40 +0530

    write function to validate password, generate accessToken and to generate refreshToken.

--- backend/src/Models/User.model.js
+++ backend/src/Models/User.model.js
@@ -4,10 +4,10 @@ import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 dotenv.config();
 
-const userSchema  = mongoose.Schema({
+const userSchema  = new mongoose.Schema({
     firstName: {
         type: String,
-        require: [true, "Firstname is required!"],
+        required: [true, "Firstname is required!"],
         minLength: [2, "Firstname must contain more than 2 letter!"],
         maxLength: [20, "Firstname must not contain more than 20 letter"],
         match: [/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'],
@@ -23,15 +23,15 @@ const userSchema  = mongoose.Schema({
     userName: {
         type: String,
         unique: true,
-        require: [true, "Username is required!"],
+        required: [true, "Username is required!"],
         minLength: [2, "Firstname must contain more than 2 letter!"],
         maxLength: [20, "Firstname must not contain more than 20 letter"],
         match: [/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores']
     },
     email: {
-        typr: String,
+        type: String,
         unique: true,
-        require: true,
+        required: true,
         match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email'],
         lowercase: true
     },
@@ -42,8 +42,8 @@ const userSchema  = mongoose.Schema({
         select: false
     },
     credit: {
-        type: [Integer, "Input must be a integer number!"],
-        require: true
+        type: Number,
+        required: true
     },
     subscription: {
         type: String,
@@ -55,9 +55,61 @@ const userSchema  = mongoose.Schema({
         required: true,
         default: 'User'
     },
-    paymentHistory: {
+    paymentHistory: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Payment'
-    }
+    }]
+},
+{timestamps: true}
+)
 
+
+
+// implement the logic before saving the code in the DB
+userSchema.pre('save', async function(next){
+    try {
+        // this refers to the user document being saved
+        if(!this.isModified('password')) return next();
+
+        const salt = bcrypt.genSalt(10); // generate a salt, a unique string use to make hash more secure
+        this.password = bcrypt.hash(this.password, salt) // save new hash password 
+        next() // go to new next step (middleware)
+    } catch (err) {
+        console.log("password hasing failed!", err)
+        next(err) // send the err to next middleware
+    }
 })
+
+// compare the password with the save password in the DB
+userSchema.methods.isPasswordCorrect = async function(password){
+    try {
+        return bcrypt.compare(password, this.password) // compare the password with the store password hash
+    } catch (err) {
+        throw new Error('password validation failed!') // throw a new error
+    }
+}
+
+// generate a refresh token
+userSchema.methods.genRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id
+    }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
+// generate a accessToken
+userSchema.methods.genAccessToken = function(){
+    return jwt.sign({
+        _id : this._id,
+        username : this.userName,
+        email : this.email
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+
+const User = mongoose.model('User', userSchema);
+
+export {User};
