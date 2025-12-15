801e136b6b423cf4be82007b76e85717abe9f1e6
anurag0577 <anurag0577@gmail.com>
2025-12-15 06:02:28 +0530

    Fix: Header showing loading when reload.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -44,10 +44,6 @@ export default function Header() {
     staleTime: 1000 * 60 * 5, 
   });
 
-  if(isLoading){
-    return <div>Loading...</div>;
-  }
-
   if(isError){
     console.error("Error fetching user info");
   }   
@@ -57,36 +53,28 @@ export default function Header() {
   }
 
   return (
-    <header className=" px-4 md:px-4 border-b">
-      <div className="flex h-16 items-center justify-between gap-4  rounded-lg p-4 ">
+    <header className=" px-4 border-b">
+      <div className="flex flex-col items-center justify-start md:flex-row md:h-16 md:justify-between md:gap-4  rounded-lg md:p-4 ">
         {/* Logo */}
-        <div className="flex-1">
-          <a href="#" className="text-primary hover:text-primary/90 font-bold">
+        <div className="md:flex-1">
+          <a href="/" onClick={(e) => { e.preventDefault(); navigate('/'); }} className="text-black dark:text-primary text-2xl font-bold">
             Mockly
           </a>
         </div>
         {/* Right side */}
         {(!isAuthenticated)?(
-            <div className="flex flex-1 items-center justify-end gap-2">
-                <CreditButton userInfo={userInfo}/>
-            {/* <Button asChild variant="ghost" size="sm" className="text-sm                
-                <CreditButton userInfo={userInfo} />Login</a>
-            </Button> */}
-            <LoginDialogueBox/>
-            <SignUpDialogueBox/>
-            {/* <Button asChild size="sm" className="text-sm">
-                <a href="#">Signup</a>
-            </Button> */}
-            
-            <ThemeSwitch />
-            </div>
+            <div className="flex items-center py-1 w-full gap-2 md:py-0 md:justify-end md:w-fit">
+              <LoginDialogueBox className="flex-1" /> 
+              <SignUpDialogueBox />
+              <ThemeSwitch  />
+          </div>
         ) 
         :
         (
-            <div className="flex flex-1 items-center justify-end gap-2">
-                <CreditButton userInfo={userInfo} />
-                <Button asChild size="sm" className="text-sm">
-                    <a href="#" onClick={() => navigate('/buy-credits')}>
+            <div className="flex items-center py-1 w-full gap-2 md:py-0 md:justify-end md:w-fit">
+                <CreditButton userInfo={userInfo}  />
+                <Button asChild size="sm" className="text-sm flex-1">
+                    <a href="#" onClick={(e) => { e.preventDefault(); navigate('/buy-credits'); }}> 
                         <SparklesIcon className="-me-1 opacity-60" size={16} aria-hidden="true" />
                         Buy Credits
                     </a>
